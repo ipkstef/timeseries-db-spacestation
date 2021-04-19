3eed9802086fdacb88a6bd034a6e178e307d0b2d
Stefanos Amanuel <stefano.amanuel@me.com>
2021-04-19 01:45:23 -0400

    added while loop with 5 second pause

--- src/app.py
+++ src/app.py
@@ -3,17 +3,27 @@ import os
 import requests 
 import json
 from datetime import datetime
+import time
 
 
 
 database = os.path.relpath('./data/iss_timeseries.db')
 
 url = "http://api.open-notify.org/iss-now.json"
-response = requests.get(url)
-obj = json.loads(response.content)
+
 
 def main():
-	create_user.write_user(obj['timestamp'],obj['iss_position']['latitude'], obj['iss_position']['latitude'],datetime.now().date())
+	while True:
+		try:
+			response = requests.get(url)
+			obj = json.loads(response.content)
+		except requests.ConnectionError as e:
+			print(e, + "this is in response requests error")
+		else:
+			#write to db
+			create_user.write_user(obj['timestamp'],obj['iss_position']['latitude'], obj['iss_position']['latitude'],datetime.now().date())
+		time.sleep(5)
+
 
 
 
