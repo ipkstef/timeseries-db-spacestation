3aafd1a92c6e03e3d9ed769a3364afee42abffea
Stefanos Amanuel <stefano.amanuel@me.com>
2021-04-19 01:30:18 -0400

    fixes???

--- src/app.py
+++ src/app.py
@@ -1,18 +1,19 @@
 from data import create_user
 import os
-import urllib.request as urllib2
+import requests 
 import json
 from datetime import datetime
 
 
+
 database = os.path.relpath('./data/iss_timeseries.db')
 
-req = urllib2.Request("http://api.open-notify.org/iss-now.json")
-response = urllib2.urlopen(req)
-obj = json.loads(response.read())
+url = "http://api.open-notify.org/iss-now.json"
+response = requests.get(url)
+obj = json.loads(response.content)
 
 def main():
-		create_user.write_user(obj['timestamp'],obj['iss_position']['latitude'], obj['iss_position']['latitude'],datetime.now().date())
+	create_user.write_user(obj['timestamp'],obj['iss_position']['latitude'], obj['iss_position']['latitude'],datetime.now().date())
 
 
 
