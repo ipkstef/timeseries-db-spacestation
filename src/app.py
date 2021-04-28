a6963da2a0e02ae690e78693151047e23677e843
yes <email@email.org>
2021-04-27 21:19:54 -0400

    some basic logging when it crashes

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ import requests
 import json
 from datetime import datetime
 import time
+import logging
 
 
 
 database = os.path.relpath('./data/iss_timeseries.db')
 
 url = "http://api.open-notify.org/iss-now.json"
-
+logging.basicConfig(format='%(levelname)s:%(asctime)s: %(message)s', filename='myapp.log', level=logging.INFO)
 
 def main():
 	while True:
@@ -19,9 +20,11 @@ def main():
 			obj = json.loads(response.content)
 		except requests.ConnectionError as e:
 			print(e, + "this is in response requests error")
+			logging.error('Connection Error to the api')
 		else:
 			#write to db
 			create_user.write_user(obj['timestamp'],obj['iss_position']['latitude'], obj['iss_position']['longitude'],datetime.now().date())
+			
 		time.sleep(5)
 
 
@@ -30,4 +33,7 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except Exception as e:
+		logging.critical('This is a hard crash, db missing or someshit ')
